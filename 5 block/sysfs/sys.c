bfa98cf1e8885acb7479f8e2a186df35ff3c57a4
Librenoco <warnet1920@gmail.com>
2021-08-31 13:07:24 +0700

    sys

--- 5 block/sysfs/sys.c
+++ 5 block/sysfs/sys.c
@@ -1,10 +1,10 @@
-#include <linux/module.h> 
-#include <linux/printk.h> 
+#include <linux/module.h>
+#include <linux/printk.h>
 #include <linux/kobject.h>
-#include <linux/sysfs.h> 
-#include <linux/init.h> 
-#include <linux/fs.h> 
-#include <linux/string.h> 
+#include <linux/sysfs.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/string.h>
 
 static struct kobject *example_kobject;
 static int foo;
@@ -14,7 +14,7 @@ static ssize_t foo_show(struct kobject *kobj, struct kobj_attribute *attr, char 
         return sprintf(buf, "%d\n", foo);
 }
 
-static ssize_t foo_store(struct kobject *kobj, struct kobj_attribute *attr, char *buf, size_t count)
+static ssize_t foo_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
         sscanf(buf, "%du", &foo);
         return count;
